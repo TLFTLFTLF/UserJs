bac7fa187dfad24558b6774e1b6392d1204de9a7
TLF <769311201@qq.com>
2026-02-23 15:44:33 +0800

    some fixes

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -385,11 +385,11 @@
           }
         },
         getLast: function () {
-          const v = localStorage.getItem((isIsekai ? 'hvuti' : 'hvut') + '_last_post');
+          const v = window.localStorage.getItem((isIsekai ? 'hvuti' : 'hvut') + '_last_post');
           return v === null ? undefined : JSON.parse(v);
         },
         setLast: function (last) {
-          localStorage.setItem((isIsekai ? 'hvuti' : 'hvut') + '_last_post', JSON.stringify(last));
+          window.localStorage.setItem((isIsekai ? 'hvuti' : 'hvut') + '_last_post', JSON.stringify(last));
         },
         timer: function () {
           function ontimer() {
